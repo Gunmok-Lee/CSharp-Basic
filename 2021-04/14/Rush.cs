1de966c354f7e8d24417b84e69b609d1c6a6e6c4
Gunmok-Lee <gunmok.l@gmail.com>
2021-04-14 21:27:39 +0900

    2021. 4. 14 study

---
+++ 2021-04/14/Rush.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[] {};
+        for(int i = 0; i < n; i++)
+        {
+            answer[i + 1] = 
+        }
+        return answer;
+    }
+}
