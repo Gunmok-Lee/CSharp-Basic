63a31395692e2d217246cf65807c7b252baa5a1b
Gunmok-Lee <gunmok.l@gmail.com>
2021-04-17 14:07:25 +0900

    2021. 4. 17 (2)

--- 2021-04/11/Harshad.cs
+++ 2021-04/11/Harshad.cs
@@ -1,1 +1,19 @@
 using System;
+
+public class Solution {
+    public bool solution(int x) {
+        bool answer = true;
+        string a = x.ToString();
+        int sum = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            sum += Int32.Parse(a.Substring(i,1));
+        }
+
+        if (x % sum /= 0)
+        {
+            answer = false;
+        }
+        return answer;
+    }
+}
